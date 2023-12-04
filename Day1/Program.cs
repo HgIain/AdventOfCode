7930dc6b3a70d3b9ef46e6aee57051d7b9db8e9f
IainHg <iain@hellogames.co.uk>
2023-12-04 13:56:51 +0000

    Refactored regex to use right-to-left matching to find last digit

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,35 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text.RegularExpressions;
 
-var text = await File.ReadAllLinesAsync("aocinput.txt");
+var text = await File.ReadAllLinesAsync("testinput.txt");
 
 int total = 0;
 
-var regex1 = new Regex(@"\D*(\d).*(\d)\D*", new RegexOptions { });
-var regex2 = new Regex(@"\D*(\d)\D*", new RegexOptions { });
+var regexString = @"(\d)";
+var regex1 = new Regex(regexString);
+var regex2 = new Regex(regexString, RegexOptions.RightToLeft);
 
 foreach (var line in text)
 {
-    var regexResult = regex1.Match(line);
     int value = 0;
 
-    if (regexResult.Success)
+    var regexResult = regex1.Match(line);
+    if (!regexResult.Success)
     {
-        // we have two groups, so we have two numbers
-        value += int.Parse(regexResult.Groups[1].Value) * 10;
-        value += int.Parse(regexResult.Groups[2].Value);
+        throw new Exception("Regex failed");
     }
-    else
-    {
-        regexResult = regex2.Match(line);
-        if (!regexResult.Success)
-        {
-            throw new Exception("Regex failed");
-        }
+    value += int.Parse(regexResult.Groups[1].Value) * 10;
 
-        value += int.Parse(regexResult.Groups[1].Value);
-        value += int.Parse(regexResult.Groups[1].Value) * 10;
+    regexResult = regex2.Match(line);
+    if (!regexResult.Success)
+    {
+        throw new Exception("Regex failed");
     }
+    value += int.Parse(regexResult.Groups[1].Value);
 
     Console.WriteLine($"Value: {value}");
     total += value;
