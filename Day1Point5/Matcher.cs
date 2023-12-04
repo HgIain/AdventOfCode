55415a07b30d6ca3efa8185f530e524657e7dd5a
IainHg <iain@hellogames.co.uk>
2023-12-04 13:51:41 +0000

    day1.5 done

---
+++ Day1Point5/Matcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Point5
+{
+    internal class Matcher
+    {
+        static readonly Dictionary<string, int> _numbers = new Dictionary<string, int>
+        {
+            { "zero", 0 },
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 },
+            { "four", 4 },
+            { "five", 5 },
+            { "six", 6 },
+            { "seven", 7 },
+            { "eight", 8 },
+            { "nine", 9 },
+        };
+
+        public static int Match(string line)
+        {
+            if(_numbers.TryGetValue(line, out int number))
+            {
+                return(number);
+            }
+
+            return int.Parse(line);
+        }
+    }
+}
---
+++ Day1Point5/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day1Point5;
+using System.Text.RegularExpressions;
+
+var text = await File.ReadAllLinesAsync("aocinput.txt");
+
+int total = 0;
+
+var regexString = @"(zero|one|two|three|four|five|six|seven|eight|nine|\d)";
+var regex = new Regex(regexString);
+var regexBack = new Regex(regexString, RegexOptions.RightToLeft );
+
+foreach (var line in text)
+{
+    var regexResult = regex.Match(line);
+    int value = 0;
+
+    if (regexResult.Success)
+    {
+        // we have two groups, so we have two numbers
+        value += Matcher.Match(regexResult.Groups[1].Value) * 10;
+    }
+
+    regexResult = regexBack.Match(line);
+
+    if (regexResult.Success)
+    {
+        // we have two groups, so we have two numbers
+        value += Matcher.Match(regexResult.Groups[1].Value);
+    }
+
+
+    Console.WriteLine($"Value: {value}");
+    total += value;
+}
+
+Console.WriteLine($"Total: {total}");
