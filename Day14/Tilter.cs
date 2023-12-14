a4b0a8fb8de0c0a553adb4d9e940dff76413edd7
Iain Brown <iain@hellogames.co.uk>
2023-12-14 13:11:01 +0000

    Day 14 pt 1

---
+++ Day14/Tilter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+    public class Tilter
+    {
+        private readonly string[] _lines;
+
+        public Tilter(string filename)
+        {
+            _lines = System.IO.File.ReadAllLines(filename);
+        }
+
+        public int GetTotalStress()
+        {
+            int total = 0;
+
+            int rowCount = _lines.Length;
+            int colCount = _lines[0].Length;
+
+            char[,] grid = new char[colCount, rowCount];
+
+            for (int y = 0; y < rowCount; y++)
+            {
+                var line = _lines[y];
+                for (int x = 0; x < colCount; x++)
+                {
+                    grid[x, y] = line[x];
+                }
+            }
+
+            for(int y = rowCount - 1; y >= 1; y--)
+            {
+                for(int x = 0; x < colCount; x++)
+                {
+                    for(int currRow = y; currRow < rowCount; currRow++)
+                    {
+                        if (grid[x,currRow] == 'O')
+                        {
+                            if (grid[x, currRow - 1] == '.')
+                            {
+                                // free space north of us, move up
+                                grid[x, currRow] = '.';
+                                grid[x, currRow - 1] = 'O';
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            int multiple = 1;
+            for (int y = rowCount - 1; y >= 0; y--, multiple++)
+            {
+                for (int x = 0; x < colCount; x++)
+                {
+                    if (grid[x, y] == 'O')
+                    {
+                        total += multiple;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Total stress {total}");
+
+            return total;
+        }
+    }
+}
---
+++ Day1Tests/Day14Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day14;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14.Tests
+{
+    [TestClass()]
+    public class Day14Tests
+    {
+        [TestMethod()]
+        public void TilterTest()
+        {
+            var tilter = new Tilter("Day14TestInput.txt");
+            var result = tilter.GetTotalStress();
+            Assert.AreEqual(136, result);
+        }
+
+        [TestMethod()]
+        public void TilterTestFullData()
+        {
+            var tilter = new Tilter("Day14Input.txt");
+            var result = tilter.GetTotalStress();
+            Assert.AreEqual(108759, result);
+        }
+    }
+}
