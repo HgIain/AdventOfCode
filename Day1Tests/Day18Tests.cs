6553d7e2b0d61a7f843f4fb04469f78dbe3171a6
Iain Brown <iain@hellogames.co.uk>
2023-12-18 16:24:25 +0000

    Day 18 unit tests

---
+++ Day1Tests/Day18Tests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day18;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18.Tests
+{
+    [TestClass()]
+    public class Day18Tests
+    {
+        [TestMethod()]
+        public void GetHoleSizeTest()
+        {
+            var poolDigger = new PoolDigger("Day18TestInput.txt", false);
+            var result = poolDigger.GetHoleSize();
+            Assert.AreEqual(62, result);
+        }
+        [TestMethod()]
+        public void GetHoleSizeTestFullData()
+        {
+            var poolDigger = new PoolDigger("Day18Input.txt", false);
+            var result = poolDigger.GetHoleSize();
+            Assert.AreEqual(92758, result);
+        }
+
+        [TestMethod()]
+        public void GetHoleSizeTestBig()
+        {
+            var poolDigger = new PoolDigger("Day18TestInput.txt", true);
+            var result = poolDigger.GetHoleSize();
+            Assert.AreEqual(952408144115, result);
+        }
+        [TestMethod()]
+        public void GetHoleSizeTestFullDataBig()
+        {
+            var poolDigger = new PoolDigger("Day18Input.txt", true);
+            var result = poolDigger.GetHoleSize();
+            Assert.AreEqual(62762509300678, result);
+        }
+    }
+}
